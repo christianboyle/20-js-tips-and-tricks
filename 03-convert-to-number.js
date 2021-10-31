1beaac4d2ae5bd5c5df3bb0da1c6789e261f1f84
Christian Boyle <mail@christianboyle.com>
2021-10-31 11:24:25 -0500

    add 03-convert-to-number

---
+++ 03-convert-to-number.js
@@ -0,0 +1,16 @@
+let num = '999';
+let stringNumber = Number(num);
+
+console.log(stringNumber); // 999
+console.log(typeof stringNumber); // number
+
+// Cast to Number in Javascript using the Unary (+) Operator
+//
+// https://medium.com/@nikjohn/cast-to-number-in-javascript-using-the-unary-operator-f4ca67c792ce
+// https://262.ecma-international.org/5.1/#sec-11.4.7
+
+let int = '222';
+int = +int;
+
+console.log(int); // 222
+console.log(typeof int); // number
