56152d19e4dd9aed62d322af393837c21ee786b9
Christian Boyle <mail@christianboyle.com>
2021-11-01 12:30:26 -0500

    add 04-convert-boolean-to-number

---
+++ 04-convert-boolean-to-number.js
@@ -0,0 +1,6 @@
+// The unary plus operator (+) precedes its operand and evaluates to its operand but attempts to convert it into a number, if it isn't already.
+//
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus
+
+console.log(+true); // 1
+console.log(+false); // 0
