d82b63e8793b6aa7d383d1eea16c2b994f75702d
Christian Boyle <mail@christianboyle.com>
2021-11-22 08:48:32 -0600

    add 17-divide-and-round-down

---
+++ 17-divide-and-round-down.js
@@ -0,0 +1,8 @@
+// divide any number in half and round down
+//
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Right_shift
+
+console.log(28 >> 1); // 14
+console.log(29 >> 1); // 14
+console.log(4 >> 1); // 2
+console.log(5 >> 1); // 2
