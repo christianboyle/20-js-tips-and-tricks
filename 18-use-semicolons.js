4e808459a8707e3ef2dfd400ea497e067b4d6011
Christian Boyle <mail@christianboyle.com>
2021-11-23 07:12:10 -0600

    add 18-use-semicolons

---
+++ 18-use-semicolons.js
@@ -0,0 +1,3 @@
+// instead of typing semicolons set up prettier to format on save so that they are added automatically
+//
+// https://blog.yogeshchavan.dev/automatically-format-code-on-file-save-in-visual-studio-code-using-prettier
